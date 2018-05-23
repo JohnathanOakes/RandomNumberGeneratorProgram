f0b9462f5ed7f1d09af19703ceaa2ce5acf10c5e
JohnathanOakes <johnathanoakes@Johnathans-MacBook-Pro.local>
2018-05-22 21:14:55 -0400

    Fixing the NPCItem Class.

---
+++ Oakes_Johnathan_RandomNumberGame/Oakes_Johnathan_RandomNumberGame/NPCItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oakes_Johnathan_RandomNumberGame
+{
+    class NPCItem
+    {
+        //This class will create a random item that the NPC will use.
+
+        //Member Varaiable.
+        int memNPCItem;
+        
+        //Consturctor for the class
+        public NPCItem(int _NPCItem)
+        {
+            //Setting varaiables for the paramater.
+            memNPCItem = _NPCItem;
+        }
+
+        //Getter for the class.
+        public int GetNPCItem()
+        {
+            //Returing the value for the NPC's item.
+            return memNPCItem;
+        }
+
+        //Creating the setter for the class.
+        public void SetNPCItem(int _height)
+        {
+            //Allowing the value to be changed.
+            Random randomNumber = new Random();
+            int NPCItem = randomNumber.Next(1, 3);
+            _height = NPCItem;
+            this.memNPCItem = _height;
+        }
+    }
+}
