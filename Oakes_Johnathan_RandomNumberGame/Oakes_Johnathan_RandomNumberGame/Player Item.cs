fd9c6b4a1fb5e85afdf2dfffaf88e5c7830cc546
JohnathanOakes <johnathanoakes@Johnathans-MacBook-Pro.local>
2018-05-24 21:26:52 -0400

    Adding if statements that will tell the player what item they selected.

--- Oakes_Johnathan_RandomNumberGame/Oakes_Johnathan_RandomNumberGame/Player Item.cs
+++ Oakes_Johnathan_RandomNumberGame/Oakes_Johnathan_RandomNumberGame/Player Item.cs
@@ -8,10 +8,32 @@ namespace Oakes_Johnathan_RandomNumberGame
 {
     class Player_Item
     {
+        //This class will hold the varable for the players item.
         int PlayerItem(string playeritem)
         {
-            int item =0;
-            while (!int.TryParse(playeritem, item)) ;
-        }
+            //Converting a string varable to a intager varable.
+            int item;
+            int.TryParse(playeritem, out item);
+
+            while (!(int.TryParse(playeritem, out item)) && playeritem == "1" && playeritem == "2" && playeritem == "3") ;
+            {
+                Console.WriteLine("Error the item that you selected does not exist.");
+            }
+            //Telling the player the item they selected
+            if(item == 1)
+            {
+                Console.WriteLine("You have selected the shield");
+            }
+
+            if(item == 2)
+            {
+                Console.WriteLine("You have selected the arrow");
+            }
+
+            if(item == 3)
+            {
+                Console.WriteLine("You have selected the shield.");
+            }
+            
     }
 }
